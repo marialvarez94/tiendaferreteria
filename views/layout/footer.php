c5c735b14a35211b739e3ffe2f27f71b68ce6807
maria <alvarezmaria513@gmail.com>
2020-04-24 22:20:48 -0500

    tienda

---
+++ views/layout/footer.php
@@ -0,0 +1,11 @@
+</div>
+</div>
+
+			<!-- PIE DE PÁGINA -->
+			<footer id="footer">
+				<img src="<?=base_url?>assets/img/pie.jpg" alt="Ferreteria Samir" />
+				<p>Ferreteria Samir &copy; <?= date('Y') ?></p>
+			</footer>
+		</div>
+	</body>
+</html>
